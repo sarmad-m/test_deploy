c5b20c7ea1e32ea8a5ec0d32d5ecb573d464aff7
sarmad-m <63185071+sarmad-m@users.noreply.github.com>
2024-03-19 17:25:50 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,35 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
 import pickle
-from sklearn.preprocessing import StandardScaler
 
 # Load the saved model
-model = pickle.load(open("classifier.sav", "rb"))
+with open("best_rf_model.pkl", "rb") as f:
+    model = pickle.load(f)
 
-def preprocess_data(data):
-    scaler = StandardScaler()
-    columns_to_scale = ['Pr', 'Frate', 'Favrg', 'Time', 'Vtotal', 'Fmax', 'Tmax', 'SNO']
-    data[columns_to_scale] = scaler.fit_transform(data[columns_to_scale])
-    return data
+# Title and description
+st.title('Predict Urine Flowmeter')
+st.markdown('This app predicts the class label based on the input features.')
 
-# Create a Streamlit app
-st.title("Urine Flowmeter Prediction App")
+# Input features
+st.header("Input Features")
+Pr = st.slider("Pr", min_value=0, max_value=100, step=1)
+Frate = st.number_input("Frate")
+Favrg = st.number_input("Favrg")
+Time = st.number_input("Time")
+Vtotal = st.number_input("Vtotal")
+Fmax = st.number_input("Fmax")
+Tmax = st.number_input("Tmax")
+SNO = st.number_input("SNO")
 
-# Collect user input
-with st.form("form"):
-    Pr = st.slider("Pr", min_value=0, max_value=100, step=1)
-    Frate = st.number_input("Frate")
-    Favrg = st.number_input("Favrg")
-    Time = st.number_input("Time")
-    Vtotal = st.number_input("Vtotal")
-    Fmax = st.number_input("Fmax")
-    Tmax = st.number_input("Tmax")
-    SNO = st.number_input("SNO")
-    submitted = st.form_submit_button("Predict")
-
-if submitted:
-    # Preprocess input data
-    new_data = pd.DataFrame([[Pr, Frate, Favrg, Time, Vtotal, Fmax, Tmax, SNO]], columns=['Pr', 'Frate', 'Favrg', 'Time', 'Vtotal', 'Fmax', 'Tmax', 'SNO'])
-    processed_data = preprocess_data(new_data)
-
+# Prediction button
+if st.button("Predict"):
     # Make prediction
-    prediction = model.predict(processed_data)
+    input_data = np.array([['Pr',	'Frate',	'Favrg',	'Time',	'Vtotal',	'Fmax'	,'Tmax',	'SNO']])
+    prediction = model.predict(input_data)
+    st.write("Predicted Class:", prediction[0])
 
-    # Display the predicted class
-    st.write("Predicted Class:", prediction)
+st.text('')
+st.text('')
+st.markdown(
+    '`Initial code was developed by` [santiviquez](https://twitter.com/santiviquez) | \
+         `Code modification and update by:` [Mohamed Alie](https://github.com/Kmohamedalie/iris-streamlit/tree/main)')
